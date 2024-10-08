f7a44a3284576d277a4926078edc92365d76fc2f
Alefale08 <134966756+Alefale08@users.noreply.github.com>
2024-10-08 18:41:35 +0200

    Madblid

---
+++ madlib.py
@@ -0,0 +1,9 @@
+#Easy project python 1(Practice)
+adj=input("Adjective: ")
+verb1=input("Verb: ")
+verb2=input("Verb: ")
+famous_person=input("Famous person: ")
+
+madlib="Computer programming is so "+adj+" It makes me so excited all the time because\
+    I love to "+verb1+" . Stay hydrated and "+verb2+" like you are "+famous_person+" !"
+print(madlib)
